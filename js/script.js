e6565952c01874b9fe88074665d19b62b6e8b9e6
Abdullah-Al-Tamim-573 <abdullahaltamim573@gmail.com>
2026-03-09 14:47:41 +0600

    add badge dynamically in boxexs and modal

--- js/script.js
+++ js/script.js
@@ -9,17 +9,17 @@ let displayDataArray = [];
 
 
 let loadGithubIssuesData = async (command) => {
-    let url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
-    let fetchIsseueResponse = await fetch(url);
-    let issueData = await fetchIsseueResponse.json();
+  let url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+  let fetchIsseueResponse = await fetch(url);
+  let issueData = await fetchIsseueResponse.json();
 
 
-    openFilterBtn.classList.remove("btn-primary");
-    closeFilterBtn.classList.remove("btn-primary");
-    displayDataArray = issueData.data;
-    dispalyGithubIssuesData();
-    issuesCount.innerText = displayDataArray.length
-    filterIssuesData(command, issueData.data);
+  openFilterBtn.classList.remove("btn-primary");
+  closeFilterBtn.classList.remove("btn-primary");
+  displayDataArray = issueData.data;
+  dispalyGithubIssuesData();
+  issuesCount.innerText = displayDataArray.length
+  filterIssuesData(command, issueData.data);
 
 }
 
@@ -43,28 +43,30 @@ let filterBoxesMainParent = document.getElementById("filter-boxes-main-parent");
 // }
 
 let dispalyGithubIssuesData = () => {
-    filterBoxesMainParent.innerHTML = "";
-    displayDataArray.forEach(data => {
+  filterBoxesMainParent.innerHTML = "";
+  let bugParent = document.getElementById("tags-parent");
+  displayDataArray.forEach(data => {
 
-        let { id, title, description, status, labels, priority, author, assignee, createdAt, updatedAt } = data;
+    let { id, title, description, status, labels, priority, author, assignee, createdAt, updatedAt } = data;
 
-        let formatedCreatedAt = new Date(createdAt).toLocaleDateString()
-        let formatedupdatedAt = new Date(updatedAt).toLocaleDateString()
-        let createBoxDiv = document.createElement("div");
-        createBoxDiv.className = `
+
+
+
+    let formatedCreatedAt = new Date(createdAt).toLocaleDateString();
+    let formatedupdatedAt = new Date(updatedAt).toLocaleDateString();
+    let createBoxDiv = document.createElement("div");
+    createBoxDiv.className = `
         box shadow-2xl border-t-5 ${status === 'open' ? 'border-t-[#00a96e]' : 'border-t-[#a755f6]'}  space-y-2 rounded-lg`;
 
-        createBoxDiv.setAttribute(`onclick`, `loadBoxModal(${id})`)
+    createBoxDiv.setAttribute(`onclick`, `loadBoxModal(${id})`)
 
-
-
-        createBoxDiv.innerHTML = `
+    createBoxDiv.innerHTML = `
         <div class="py-6 px-7 space-y-4">
             <!-- ................................ 1-->
             <div class="flex justify-between">
               ${status === 'open' ?
-                '<img src="../assets/Open-Status.png" alt="Open Status" />'
-                : '<img src="../assets/Closed-Status.png" alt="Closed Status" />'}
+        '<img src="../assets/Open-Status.png" alt="Open Status" />'
+        : '<img src="../assets/Closed-Status.png" alt="Closed Status" />'}
               <div
                 class="badge badge-soft font-medium text-[12px] text-[#EF4444] bg-[#feecec] px-5 rounded-full"
               >
@@ -91,33 +93,21 @@ let dispalyGithubIssuesData = () => {
             <!-- ................................ 4-->
 
             <div class="flex gap-1">
-              <!-- 1st part -->
-              <div>
-                <div
-                  class="badge badge-soft border-[#EF4444] font-medium  text-[#EF4444] bg-[#feecec] px-4 rounded-full"
-                >
-                  <img src="../assets/BugDroid.png" alt="BugDroid" />
-                  <span class="text-[12px]">Bug</span>
-                </div>
-              </div>
-              <!-- 2nd part -->
-              <div>
-                <div
-                  class="badge badge-soft border-2 border-[#ffdd56] badge-warning rounded-full px-4  bg-[#fff8db]"
-                >
-                  <img src="../assets/Lifebuoy.png" alt="Lifebuoy" />
-                  <span class="text-[12px]">help wanted</span>
-                </div>
-              </div>
+                   ${labels
+        .map(label => `
+              <span class="badge badge-soft font-medium text-[#EF4444] bg-[#feecec] border-[#EF4444] px-3 rounded-full text-[12px]">
+                  ${label}
+             </span>
+       `).join("")
+        }
             </div>
-          </div>
 
           <!-- ................................ 5-->
           <hr />
           <div class="px-5 space-y-1 py-3 flex justify-between items-center">
             <div>
                     <div class="text-[#64748B] text-[10px]">#1by ${author}</div>
-                    <div class="text-[#64748B] text-[10px]">Assignee:${assignee? assignee : 'no one'}</div>
+                    <div class="text-[#64748B] text-[10px]">Assignee:${assignee ? assignee : 'no one'}</div>
                      
             </div>
             <div>
@@ -129,18 +119,27 @@ let dispalyGithubIssuesData = () => {
           </div>
        
        `
-        filterBoxesMainParent.appendChild(createBoxDiv)
+    filterBoxesMainParent.appendChild(createBoxDiv);
 
-    })
+
+
+
+
+
+  })
+
+
+
+
 }
 
 
 // load card modal 
 
 let loadBoxModal = async (id) => {
-    let boxModalResponse = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
-    let boxModalData = await boxModalResponse.json();
-    displayBoxModal(boxModalData.data)
+  let boxModalResponse = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+  let boxModalData = await boxModalResponse.json();
+  displayBoxModal(boxModalData.data)
 
 }
 
@@ -161,10 +160,10 @@ let loadBoxModal = async (id) => {
 // }
 
 let displayBoxModal = (boxModalData) => {
-    let { title, description, status, labels, priority, assignee, createdAt } = boxModalData
-    let dialog = document.getElementById("my_modal_5");
-    let createdAtFormatDate = new Date(createdAt).toLocaleDateString()
-    dialog.innerHTML = `
+  let { title, description, status, labels, priority, assignee, createdAt } = boxModalData
+  let dialog = document.getElementById("my_modal_5");
+  let createdAtFormatDate = new Date(createdAt).toLocaleDateString()
+  dialog.innerHTML = `
              
               <div class="modal-box space-y-5 p-7">
               <div>
@@ -178,32 +177,15 @@ let displayBoxModal = (boxModalData) => {
               </span>
               <ul class="flex gap-2 items-center">
                 <li class="text-[11px] text-[#64748B] list-disc list-inside">
-                Opened by <span>${assignee? assignee : 'no one'}</span></li>
+                Opened by <span>${assignee ? assignee : 'no one'}</span></li>
                 <li class="text-[11px] text-[#64748B] list-disc list-inside">${createdAtFormatDate}</li>
               </ul>
             </div>
               </div>
             
             <!-- 3rd modal part -->
-            <div class="flex gap-1">
-              <!-- 1st part -->
-              <div>
-                <div
-                  class="badge badge-soft border-[#EF4444] font-medium text-[#EF4444] bg-[#feecec] px-4 rounded-full"
-                >
-                  <img src="../assets/BugDroid.png" alt="BugDroid" />
-                  <span class="text-[12px]">Bug</span>
-                </div>
-              </div>
-              <!-- 2nd part -->
-              <div>
-                <div
-                  class="badge badge-soft border-2 border-[#ffdd56] badge-warning rounded-full px-4 bg-[#fff8db]"
-                >
-                  <img src="../assets/Lifebuoy.png" alt="Lifebuoy" />
-                  <span class="text-[12px]">help wanted</span>
-                </div>
-              </div>
+            <div class="flex gap-1 modal-badge-parent">
+                      
             </div>
             <!-- 4th modal part -->
             <div class="text-[#64748B]">${description}</div>
@@ -212,7 +194,7 @@ let displayBoxModal = (boxModalData) => {
                         <!-- 1st part -->
                          <div >
                               <p class="text-[#64748B]">Assignee:</p>
-                              <p class="font-semibold text-[#1F2937]">${assignee? assignee : 'no one'}</p>
+                              <p class="font-semibold text-[#1F2937]">${assignee ? assignee : 'no one'}</p>
                          </div>
 
                          <!-- 2nd part -->
@@ -233,50 +215,58 @@ let displayBoxModal = (boxModalData) => {
           </div>
         
         `
-    dialog.showModal()
+
+        let modalBadgeParent = document.querySelector(".modal-badge-parent");
+        labels.forEach(data => {
+          let span = document.createElement("span");
+          span.classList ="badge badge-soft border-[#EF4444] font-medium text-[#EF4444] bg-[#feecec] px-4 rounded-full";
+          span.innerText = data;
+          modalBadgeParent.appendChild(span);
+        })
+  dialog.showModal()
 }
 
 
 
 let filterIssuesData = (command, issueData) => {
 
-    let filterBtns = document.querySelectorAll(".filter-btn");
+  let filterBtns = document.querySelectorAll(".filter-btn");
 
-    if (command === 'all') {
-        filterBtns.forEach(btn => {
-            btn.classList.remove("btn-primary");
-        });
+  if (command === 'all') {
+    filterBtns.forEach(btn => {
+      btn.classList.remove("btn-primary");
+    });
 
-        allFilterBtn.classList.add("btn-primary");
-        issuesCount.innerText = displayDataArray.length
-        dispalyGithubIssuesData();
+    allFilterBtn.classList.add("btn-primary");
+    issuesCount.innerText = displayDataArray.length
+    dispalyGithubIssuesData();
 
-    }
+  }
 
-    else if (command === 'open') {
-        filterBtns.forEach(btn => {
-            btn.classList.remove("btn-primary")
-        });
-        openFilterBtn.classList.add("btn-primary");
+  else if (command === 'open') {
+    filterBtns.forEach(btn => {
+      btn.classList.remove("btn-primary")
+    });
+    openFilterBtn.classList.add("btn-primary");
 
-        let openDataFilter = displayDataArray.filter(data => data.status === command);
-        displayDataArray = openDataFilter;
-        issuesCount.innerText = displayDataArray.length
-        dispalyGithubIssuesData()
+    let openDataFilter = displayDataArray.filter(data => data.status === command);
+    displayDataArray = openDataFilter;
+    issuesCount.innerText = displayDataArray.length
+    dispalyGithubIssuesData()
 
-    }
-    else if (command === 'closed') {
-        filterBtns.forEach(btn => {
-            btn.classList.remove("btn-primary")
-        });
+  }
+  else if (command === 'closed') {
+    filterBtns.forEach(btn => {
+      btn.classList.remove("btn-primary")
+    });
 
-        closeFilterBtn.classList.add("btn-primary");
+    closeFilterBtn.classList.add("btn-primary");
 
-        let closedDataFilter = displayDataArray.filter(data => data.status === command);
-        displayDataArray = closedDataFilter;
-        issuesCount.innerText = displayDataArray.length
-        dispalyGithubIssuesData()
-    }
+    let closedDataFilter = displayDataArray.filter(data => data.status === command);
+    displayDataArray = closedDataFilter;
+    issuesCount.innerText = displayDataArray.length
+    dispalyGithubIssuesData()
+  }
 }
 
 
